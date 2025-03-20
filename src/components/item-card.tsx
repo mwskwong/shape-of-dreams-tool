7d2a2a91e8eccdfdfd8b1ebab7c2f2d7f0b25cb1
Matthew Kwong <matthew.ws.kwong@hkjc.org.hk>
2025-03-20 16:41:40 +0800

    feat: add sprites

--- src/components/item-card.tsx
+++ src/components/item-card.tsx
@@ -14,6 +14,7 @@ import { Card, type CardProps } from "@radix-ui/themes/components/card";
 import { Flex } from "@radix-ui/themes/components/flex";
 import { Heading } from "@radix-ui/themes/components/heading";
 import { Text } from "@radix-ui/themes/components/text";
+import parse, { Element } from "html-react-parser";
 import Image from "next/image";
 import { type FC } from "react";
 
@@ -100,38 +101,34 @@ export const ItemCard: FC<ItemCardProps> = ({
             Unlock by: {unlockBy}
           </Text>
         )}
-        <Text
-          as="p"
-          dangerouslySetInnerHTML={{
-            __html: description
+        <Text as="p">
+          {parse(
+            description
               .replaceAll("\n", "<br>")
               .replaceAll(
                 /<color=(.*?)>(.*?)<\/color>/g,
-                (_, color: string, content: string) => {
-                  let mappedColor;
-                  switch (color.toLowerCase()) {
-                    case "#ff8a2d": {
-                      mappedColor = "var(--orange-a11)";
-                      break;
-                    }
-                    case "#16d7ff": {
-                      mappedColor = "var(--cyan-a11)";
-                      break;
-                    }
-                    case "yellow": {
-                      mappedColor = "var(--yellow-a11)";
-                      break;
-                    }
-                    default: {
-                      mappedColor = color;
-                    }
-                  }
-
-                  return `<em style="font-family: var(--font-ibm-plex-serif); color: ${mappedColor}">${content}</em>`;
-                },
+                '<em style="font-family: var(--font-ibm-plex-serif); color: $1">$2</em>',
+              )
+              .replaceAll(
+                /<sprite=(\d+)>/g,
+                '<img src="/images/$1.png" alt="sprite $1" />',
               ),
-          }}
-        />
+            {
+              replace: (domNode) => {
+                if (domNode instanceof Element && domNode.name === "img") {
+                  return (
+                    <Image
+                      alt={domNode.attribs.alt}
+                      height={16}
+                      src={domNode.attribs.src}
+                      width={16}
+                    />
+                  );
+                }
+              },
+            },
+          )}
+        </Text>
         {tags.length > 0 && (
           <Flex gap="2" mt="auto" wrap="wrap">
             {tags.map((tag) => (
