3d46ae85e6fcce74dd33cf58529dfa01d48c1d5d
mwskwong <matthewkwong2@gmail.com>
2025-03-24 13:58:58 +0800

    feat: adding loading UI for `/essences`

---
+++ src/app/essences/layout.tsx
@@ -0,0 +1,19 @@
+import { Flex } from "@radix-ui/themes/components/flex";
+import { type FC, type PropsWithChildren } from "react";
+
+import { EssencesToolbar } from "@/components/essences/essences-toolbar";
+import { compareRarities } from "@/lib/utils";
+import essences from "@public/data/essences.json";
+
+const allRarities = [
+  ...new Set(Object.values(essences).map(({ rarity }) => rarity)),
+].toSorted(compareRarities);
+
+const EssencesLayout: FC<PropsWithChildren> = ({ children }) => (
+  <Flex direction="column" gap="3" pt="3">
+    <EssencesToolbar allRarities={allRarities} />
+    {children}
+  </Flex>
+);
+
+export default EssencesLayout;
---
+++ src/app/essences/loading.tsx
@@ -0,0 +1,18 @@
+import { Card } from "@radix-ui/themes/components/card";
+import { Grid } from "@radix-ui/themes/components/grid";
+import { Skeleton } from "@radix-ui/themes/components/skeleton";
+import { type FC } from "react";
+
+import styles from "./loading.module.css";
+
+const EssencesLayout: FC = () => (
+  <Grid columns={{ initial: "1", sm: "2", md: "3" }} gap="3">
+    {Array.from({ length: 12 }, (_, index) => (
+      <Skeleton key={index}>
+        <Card className={styles.card} />
+      </Skeleton>
+    ))}
+  </Grid>
+);
+
+export default EssencesLayout;
