a0fa8049a166d31f200279e700f6e17403a6cd3c
mwskwong <matthewkwong2@gmail.com>
2025-03-26 15:39:32 +0800

    fix: attempt to cache `/images/*`

--- next.config.ts
+++ next.config.ts
@@ -61,6 +61,15 @@ const config = {
         },
       ],
     },
+    {
+      source: "/images/:path*",
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=86400, must-revalidate",
+        },
+      ],
+    },
   ],
   // eslint-disable-next-line @typescript-eslint/require-await
   redirects: async () => [
