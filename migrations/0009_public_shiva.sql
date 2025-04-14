a856f127443ab30aa30ccda0c91bff5e854596e3
Matthew Kwong <matthewkwong2@gmail.com>
2025-04-14 21:19:19 +0800

    Revert "fix: SQL migration order"
    This reverts commit 1d06696255217df85c4a2dd64db8d2c124f97a22.

--- migrations/0009_public_shiva.sql
+++ migrations/0009_public_shiva.sql
@@ -1,5 +1,5 @@
-ALTER TABLE "builds" drop column "search_vector";--> statement-breakpoint
 ALTER TABLE "builds" RENAME COLUMN "build" TO "details";--> statement-breakpoint
 DROP INDEX "builds_build_index";--> statement-breakpoint
+ALTER TABLE "builds" drop column "search_vector";--> statement-breakpoint
 ALTER TABLE "builds" ADD COLUMN "search_vector" "tsvector" GENERATED ALWAYS AS (to_tsvector('english', ("builds"."details"->>'buildName')::text || ' ' || ("builds"."details"->>'description')::text)) STORED NOT NULL;--> statement-breakpoint
 CREATE INDEX "builds_details_index" ON "builds" USING gin ("details" jsonb_path_ops);
