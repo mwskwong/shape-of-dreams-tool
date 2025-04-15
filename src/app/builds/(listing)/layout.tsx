ce58b39d4a22889a591cd7d4dbfc3074dbb965dc
mwskwong <matthewkwong2@gmail.com>
2025-04-15 11:59:51 +0800

    fix: missing `children` in builds layout

--- src/app/builds/(listing)/layout.tsx
+++ src/app/builds/(listing)/layout.tsx
@@ -1,19 +1,18 @@
 import { Flex } from "@radix-ui/themes/components/flex";
 import { type ResolvingMetadata } from "next";
-import { type FC, Suspense } from "react";
+import { type FC, type PropsWithChildren, Suspense } from "react";
 
 import { BuildsToolbar } from "@/components/builds/builds-toolbar";
 import { routes } from "@/lib/site-config";
 
-const BuildsLayout: FC = () => {
-  return (
-    <Flex direction="column" gap="3" pt="3">
-      <Suspense>
-        <BuildsToolbar />
-      </Suspense>
-    </Flex>
-  );
-};
+const BuildsLayout: FC<PropsWithChildren> = ({ children }) => (
+  <Flex direction="column" gap="3" pt="3">
+    <Suspense>
+      <BuildsToolbar />
+    </Suspense>
+    {children}
+  </Flex>
+);
 
 export const generateMetadata = async (
   _: unknown,
