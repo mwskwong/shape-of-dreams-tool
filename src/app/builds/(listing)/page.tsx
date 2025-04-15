1321df32cd8a1960a5c4184326300fa5acefc2b7
Matthew Kwong <matthewkwong2@gmail.com>
2025-04-15 20:53:02 +0800

    feat: temp build listing UI

--- src/app/builds/(listing)/page.tsx
+++ src/app/builds/(listing)/page.tsx
@@ -1,9 +1,25 @@
+import "@/styles/traveler-colors.css";
+
+import { Avatar } from "@radix-ui/themes/components/avatar";
+import { Box } from "@radix-ui/themes/components/box";
+import { Card } from "@radix-ui/themes/components/card";
 import { Flex } from "@radix-ui/themes/components/flex";
+import { Grid } from "@radix-ui/themes/components/grid";
+import { Heading } from "@radix-ui/themes/components/heading";
+import { Text } from "@radix-ui/themes/components/text";
+import Image from "next/image";
+import Link from "next/link";
 import { type SearchParams } from "nuqs/server";
 import { type FC } from "react";
 
+import {
+  allEssenceEntries,
+  allMemoryEntries,
+  allTravelerEntries,
+} from "@/lib/constants";
 import { getBuilds } from "@/lib/queries";
-import { loadBuildSearchParams } from "@/lib/utils";
+import { routes } from "@/lib/site-config";
+import { getTravelerColor, loadBuildSearchParams } from "@/lib/utils";
 
 interface BuildsProps {
   searchParams: Promise<SearchParams>;
@@ -14,12 +30,85 @@ const Builds: FC<BuildsProps> = async ({ searchParams }) => {
   const builds = await getBuilds(parsedSearchParams);
 
   return (
-    <Flex direction="column" gap="3" pt="3">
-      search result:
-      <pre style={{ overflow: "auto" }}>
-        <code>{JSON.stringify(builds, undefined, 2)}</code>
-      </pre>
-    </Flex>
+    <Grid columns={{ initial: "1", sm: "2", md: "3" }} gap="3">
+      {builds.map((build) => {
+        const traveler = allTravelerEntries.find(
+          ([id]) => id === build.details.traveler.id,
+        )?.[1];
+
+        return (
+          <Card key={build.hashId} asChild>
+            <Link href={`${routes.builds.pathname}/${build.hashId}`}>
+              <Flex align="center" direction="column" gap="3">
+                {traveler ? (
+                  <Image
+                    alt={traveler.name}
+                    className={"rt-AvatarRoot rt-r-size-6"}
+                    height={80}
+                    src={`/images/${traveler.image}`}
+                    width={80}
+                  />
+                ) : (
+                  <Avatar color="gray" fallback="?" size="6" />
+                )}
+                <Flex align="center" direction="column">
+                  <Heading as="h2" size="4">
+                    {build.details.name}
+                  </Heading>
+                  <Text
+                    color={
+                      getTravelerColor(build.details.traveler.id) ?? "gray"
+                    }
+                  >
+                    {traveler?.name ?? "Any Traveler"}
+                  </Text>
+                </Flex>
+
+                {build.details.memories.map(({ id, essences }, index) => {
+                  const memory = allMemoryEntries.find(
+                    ([memoryId]) => memoryId === id,
+                  )?.[1];
+
+                  return (
+                    <Flex key={index} align="center" gap="3">
+                      {memory ? (
+                        <Image
+                          alt={memory.name}
+                          className={"rt-AvatarRoot rt-r-size-4"}
+                          height={48}
+                          src={`/images/${memory.image}`}
+                          width={48}
+                        />
+                      ) : (
+                        <Avatar color="gray" fallback="?" size="4" />
+                      )}
+
+                      {essences.map((id, index) => {
+                        const essence = allEssenceEntries.find(
+                          ([essenceId]) => essenceId === id,
+                        )?.[1];
+
+                        if (!essence) return <Box key={index} width="32px" />;
+
+                        return (
+                          <Image
+                            key={index}
+                            alt={essence.name}
+                            height={32}
+                            src={`/images/${essence.image}`}
+                            width={32}
+                          />
+                        );
+                      })}
+                    </Flex>
+                  );
+                })}
+              </Flex>
+            </Link>
+          </Card>
+        );
+      })}
+    </Grid>
   );
 };
 
