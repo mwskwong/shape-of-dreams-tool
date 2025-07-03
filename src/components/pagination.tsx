e4acbe402f30367e01c45291b97273155d7a9083
mwskwong <matthewkwong2@gmail.com>
2025-07-03 14:05:10 +0800

    feat: pagination UI add ellipsis handling

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -5,8 +5,13 @@ import { Flex, type FlexProps } from "@radix-ui/themes/components/flex";
 import { IconButton } from "@radix-ui/themes/components/icon-button";
 import { Spinner } from "@radix-ui/themes/components/spinner";
 import { Text } from "@radix-ui/themes/components/text";
-import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+import {
+  IconChevronLeft,
+  IconChevronRight,
+  IconDots,
+} from "@tabler/icons-react";
 import { clsx } from "clsx";
+import { range } from "lodash-es";
 import { useQueryState } from "nuqs";
 import { type FC, useEffect, useTransition } from "react";
 
@@ -14,10 +19,14 @@ import { buildSearchParams } from "@/lib/utils";
 
 import styles from "./pagination.module.css";
 
+type Item = number | "start-ellipsis" | "end-ellipsis";
 export interface PaginationProps
   extends Omit<FlexProps, "asChild" | "children"> {
   pages?: number;
 }
+
+const boundaryCount = 1;
+const siblingCount = 1;
 
 export const Pagination: FC<PaginationProps> = ({
   pages = 1,
@@ -35,6 +44,38 @@ export const Pagination: FC<PaginationProps> = ({
       window.scrollTo(0, 0);
     }
   }, [pending]);
+
+  // Ref: https://github.com/mui/material-ui/blob/master/packages/mui-material/src/usePagination/usePagination.js
+
+  const startPages = range(1, Math.min(boundaryCount, pages));
+  const endPages = range(
+    Math.max(pages - boundaryCount + 1, boundaryCount + 1),
+    pages,
+  );
+  const siblingsStart = Math.max(
+    Math.min(page - siblingCount, pages - boundaryCount - siblingCount * 2 - 1),
+    boundaryCount + 2,
+  );
+  const siblingsEnd = Math.min(
+    Math.max(page + siblingCount, boundaryCount + siblingCount * 2 + 2),
+    pages - boundaryCount - 1,
+  );
+
+  const items: Item[] = [
+    ...startPages,
+    ...(siblingsStart > boundaryCount + 2
+      ? (["start-ellipsis"] as const)
+      : boundaryCount + 1 < pages - boundaryCount
+        ? [boundaryCount + 1]
+        : []),
+    ...range(siblingsStart, siblingsEnd),
+    ...(siblingsEnd < pages - boundaryCount - 1
+      ? (["end-ellipsis"] as const)
+      : pages - boundaryCount > boundaryCount
+        ? [pages - boundaryCount]
+        : []),
+    ...endPages,
+  ];
 
   return (
     <Flex
@@ -54,18 +95,26 @@ export const Pagination: FC<PaginationProps> = ({
       >
         <IconChevronLeft size={20} />
       </IconButton>
-      {Array.from({ length: pages }, (_, index) => {
-        const active = page === index + 1;
+      {items.map((item) => {
+        if (item === "start-ellipsis" || item === "end-ellipsis") {
+          return (
+            <Box key={item} className={styles.ellipsisContainer}>
+              <IconDots size={20} />
+            </Box>
+          );
+        }
+
+        const active = page === item;
         return (
           <IconButton
-            key={index}
+            key={item}
             color={active ? undefined : "gray"}
             highContrast={!active}
             variant={active ? undefined : "ghost"}
-            onClick={() => setPage(index + 1)}
+            onClick={() => setPage(item)}
           >
             <Box asChild minWidth="20px">
-              <Text size="2">{index + 1}</Text>
+              <Text size="2">{item}</Text>
             </Box>
           </IconButton>
         );
