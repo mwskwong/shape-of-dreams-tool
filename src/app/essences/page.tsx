34ba64cced2b33de5a77fe82fde37f57c85c06f9
Matthew Kwong <matthewkwong2@gmail.com>
2025-08-22 18:29:20 +0800

    feat:: add essences toolbar

--- src/app/essences/page.tsx
+++ src/app/essences/page.tsx
@@ -1,3 +1,18 @@
-const EssencesPage = () => <div>essences page</div>;
+import { Suspense } from "react";
+
+import { EssencesToolbar } from "@/components/essences/essences-toolbar";
+import { getEssenceRarities } from "@/lib/essences";
+
+const essenceRarities = getEssenceRarities();
+
+const EssencesPage = () => {
+  return (
+    <div>
+      <Suspense>
+        <EssencesToolbar rarities={essenceRarities} />
+      </Suspense>
+    </div>
+  );
+};
 
 export default EssencesPage;
---
+++ src/components/essences/essences-toolbar.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import { Check, ChevronDown, RotateCcw, Search } from "lucide-react";
+import { useQueryStates } from "nuqs";
+import { type ComponentProps, useTransition } from "react";
+
+import { cn, itemSearchParams } from "@/lib/utils";
+
+export interface EssencesToolbarProps
+  extends Omit<ComponentProps<"div">, "children"> {
+  rarities?: string[];
+}
+
+export const EssencesToolbar = ({
+  rarities = [],
+  className,
+  ...props
+}: EssencesToolbarProps) => {
+  const [queryStates, setQueryStates] = useQueryStates({
+    search: itemSearchParams.search,
+    rarities: itemSearchParams.rarities,
+  });
+
+  const [searchPending, searchStartTransition] = useTransition();
+  const [raritiesPending, raritiesStartTransition] = useTransition();
+  const [resetPending, resetStartTransition] = useTransition();
+
+  return (
+    <div
+      className={cn("flex flex-col gap-4 md:flex-row", className)}
+      {...props}
+    >
+      <label className="input w-full sm:w-75">
+        <Search className="shrink-0" size="1.2em" />
+        <input
+          placeholder="Search..."
+          type="search"
+          value={queryStates.search}
+          onChange={(e) =>
+            setQueryStates(
+              (prev) => ({
+                ...prev,
+                search: (e.target as HTMLInputElement).value,
+              }),
+              { startTransition: searchStartTransition },
+            )
+          }
+        />
+        {searchPending && <span className="loading loading-xs shrink-0" />}
+      </label>
+
+      <div className="dropdown">
+        <div
+          className="input w-full cursor-pointer select-none sm:w-auto"
+          role="button"
+          tabIndex={0}
+        >
+          Rarity
+          {queryStates.rarities.length > 0 && (
+            <div className="badge badge-sm badge-primary nth-1:ml-auto">
+              {queryStates.rarities.length}
+            </div>
+          )}
+          {raritiesPending ? (
+            <span className="loading loading-xs nth-1:ml-auto" />
+          ) : (
+            <ChevronDown className="nth-1:ml-auto" size="1.2em" />
+          )}
+        </div>
+        <div
+          className="dropdown-content bg-base-200 rounded-box z-1 w-full p-2 shadow-sm md:w-48"
+          // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex -- needed by daisyUI to prevent focus loss
+          tabIndex={0}
+        >
+          <ul className="menu w-full">
+            {rarities.map((rarity) => (
+              <li key={rarity}>
+                <button
+                  onClick={() =>
+                    setQueryStates(
+                      (prev) => ({
+                        ...prev,
+                        rarities: prev.rarities.includes(rarity)
+                          ? prev.rarities.filter((r) => r !== rarity)
+                          : [...prev.rarities, rarity],
+                      }),
+                      { startTransition: raritiesStartTransition },
+                    )
+                  }
+                >
+                  {rarity}
+                  {queryStates.rarities.includes(rarity) && (
+                    <Check className="ml-auto" size="1.2em" />
+                  )}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button
+            className="btn btn-ghost"
+            disabled={queryStates.rarities.length === 0}
+            onClick={() =>
+              setQueryStates((prev) => ({ ...prev, rarities: [] }), {
+                startTransition: raritiesStartTransition,
+              })
+            }
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <button
+        className="btn btn-soft"
+        onClick={() =>
+          // eslint-disable-next-line unicorn/no-null
+          setQueryStates(null, { startTransition: resetStartTransition })
+        }
+      >
+        {resetPending ? (
+          <span className="loading loading-xs" />
+        ) : (
+          <RotateCcw size="1.2em" />
+        )}
+        Reset
+      </button>
+    </div>
+  );
+};
