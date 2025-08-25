03561030aed34af7efb486f9568b4f9d0a45572f
mwskwong <matthewkwong2@gmail.com>
2025-08-25 14:08:47 +0800

    feat: travelers remove unused fields and add util

--- src/lib/travelers.ts
+++ src/lib/travelers.ts
@@ -5,27 +5,6 @@ import Hero_Lacerta from "@/images/Hero_Lacerta.png";
 import Hero_Mist from "@/images/Hero_Mist.png";
 import Hero_Vesper from "@/images/Hero_Vesper.png";
 import Hero_Yubar from "@/images/Hero_Yubar.png";
-import Star_Lacerta_AttackRange from "@/images/Star_Lacerta_AttackRange.png";
-import Star_Lacerta_AttackSpeed from "@/images/Star_Lacerta_AttackSpeed.png";
-import Star_Lacerta_FireAmp from "@/images/Star_Lacerta_FireAmp.png";
-import Star_Lacerta_HandCannon from "@/images/Star_Lacerta_HandCannon.png";
-import Star_Lacerta_IncendiaryRounds from "@/images/Star_Lacerta_IncendiaryRounds.png";
-import Star_Lacerta_PrecisionShot from "@/images/Star_Lacerta_PrecisionShot.png";
-import Star_Lacerta_QuickTrigger from "@/images/Star_Lacerta_QuickTrigger.png";
-import Star_Mist_AddAttackDamage from "@/images/Star_Mist_AddAttackDamage.png";
-import Star_Mist_AddDodgeCharge from "@/images/Star_Mist_AddDodgeCharge.png";
-import Star_Mist_LightningFleche from "@/images/Star_Mist_LightningFleche.png";
-import Star_Mist_LongerLunge from "@/images/Star_Mist_LongerLunge.png";
-import Star_Mist_MovementSpeed from "@/images/Star_Mist_MovementSpeed.png";
-import Star_Mist_ParryAllDirections from "@/images/Star_Mist_ParryAllDirections.png";
-import Star_Mist_UnbreakableDeterminationHeal from "@/images/Star_Mist_UnbreakableDeterminationHeal.png";
-import Star_Yubar_AbilityPower from "@/images/Star_Yubar_AbilityPower.png";
-import Star_Yubar_EtherealInfluenceCooldownReduction from "@/images/Star_Yubar_EtherealInfluenceCooldownReduction.png";
-import Star_Yubar_IncreaseFlickerRange from "@/images/Star_Yubar_IncreaseFlickerRange.png";
-import Star_Yubar_SkillHaste from "@/images/Star_Yubar_SkillHaste.png";
-import Star_Yubar_StrongerCataclysm from "@/images/Star_Yubar_StrongerCataclysm.png";
-import Star_Yubar_StrongerSuperNova from "@/images/Star_Yubar_StrongerSuperNova.png";
-import Star_Yubar_TranquilityDamage from "@/images/Star_Yubar_TranquilityDamage.png";
 import iconAttacker from "@/images/iconAttacker.png";
 import iconSpellCaster from "@/images/iconSpellCaster.png";
 import iconSupporter from "@/images/iconSupporter.png";
@@ -42,6 +21,7 @@ const getTravelerClassIcon = (travelerClass: string) => {
   if (travelerClass.toLowerCase().includes("support")) return iconSupporter;
 };
 
+// TODO: constellations expected to be reworked, see how things goes in the official release
 const travelers = {
   Hero_Lacerta: {
     name: "Lacerta",
@@ -74,50 +54,7 @@ const travelers = {
     achievementName: "",
     achievementDescription: "",
     image: Hero_Lacerta,
-    constellations: [
-      {
-        name: "Aimed Shot I/II/III",
-        description:
-          "Lacerta's <color=yellow>Attack Speed</color> is increased by 5/12/20%.",
-        image: Star_Lacerta_AttackSpeed,
-      },
-      {
-        name: "Breath of Barasas I/II/III",
-        description:
-          "Increase the <color=yellow>Burn</color> damage over time dealt by Lacerta by 10/20/40%.",
-        image: Star_Lacerta_FireAmp,
-      },
-      {
-        name: "Sixth Sense",
-        description:
-          "Lacerta's <color=yellow>Attack Range</color> is increased by 1.",
-        image: Star_Lacerta_AttackRange,
-      },
-      {
-        name: "Smokeless Powder",
-        description:
-          "Increases <color=yellow>Hand Cannon</color>'s damage by 30% and slows enemies by 50% for 1.5 seconds.",
-        image: Star_Lacerta_HandCannon,
-      },
-      {
-        name: "Incendiary Rounds",
-        description:
-          "Increases the number of shots fire by <color=yellow>Incendiary Rounds</color> by 1.",
-        image: Star_Lacerta_IncendiaryRounds,
-      },
-      {
-        name: "Hair Trigger",
-        description:
-          "<color=yellow>Quick Draw</color> charges are restored all at once.",
-        image: Star_Lacerta_QuickTrigger,
-      },
-      {
-        name: "Composure and Focus",
-        description:
-          "Increases <color=yellow>Precision Shot</color>'s charge speed by 40%.",
-        image: Star_Lacerta_PrecisionShot,
-      },
-    ],
+    constellations: [],
   },
   Hero_Mist: {
     name: "Mist",
@@ -150,50 +87,7 @@ const travelers = {
     achievementName: "",
     achievementDescription: "",
     image: Hero_Mist,
-    constellations: [
-      {
-        name: "Astrid's Forging I/II/III",
-        description:
-          "<color=yellow>Attack Damage</color> is increased by 3/6/12%.",
-        image: Star_Mist_AddAttackDamage,
-      },
-      {
-        name: "Nimble Body I/II/III",
-        description:
-          "Increases <color=yellow>Movement Speed</color> by 5/7/9%.",
-        image: Star_Mist_MovementSpeed,
-      },
-      {
-        name: "Exceptional Agility",
-        description:
-          "Increases the maximum number of <color=yellow>Dodge</color> charges by 1.",
-        image: Star_Mist_AddDodgeCharge,
-      },
-      {
-        name: "West Wind",
-        description:
-          "Increases <color=yellow>Lunge</color>'s maximum dash distance by 33%.",
-        image: Star_Mist_LongerLunge,
-      },
-      {
-        name: "Lightning Breath",
-        description:
-          "<color=yellow>Flèche</color> now teleports instead of dashing, and deals Light Damage.",
-        image: Star_Mist_LightningFleche,
-      },
-      {
-        name: "Two Hearts",
-        description:
-          "When casting <color=yellow>Unbreakable Determination</color>, recover 25% of missing health.",
-        image: Star_Mist_UnbreakableDeterminationHeal,
-      },
-      {
-        name: "The Best Offense is Defense",
-        description:
-          "<color=yellow>Parry</color> can now deflect attacks from all directions.",
-        image: Star_Mist_ParryAllDirections,
-      },
-    ],
+    constellations: [],
   },
   Hero_Yubar: {
     name: "Yubar",
@@ -226,50 +120,7 @@ const travelers = {
     achievementName: "Pure Imagination",
     achievementDescription: "Acquire a total of 300 Dream Dust.",
     image: Hero_Yubar,
-    constellations: [
-      {
-        name: "Night Sky Silk I/II/III",
-        description:
-          "Increases <color=yellow>Ability Power</color> by 5/10/15%.",
-        image: Star_Yubar_AbilityPower,
-      },
-      {
-        name: "Stellar Flow I/II/III",
-        description:
-          "<color=yellow>Memory Haste</color> is increased by 3/7/12 reducing the cooldown of all my memories by 3/7/11%.",
-        image: Star_Yubar_SkillHaste,
-      },
-      {
-        name: "Now You See Me",
-        description:
-          "Increases the maximum travel distance of <color=yellow>Flicker</color> by 35%.",
-        image: Star_Yubar_IncreaseFlickerRange,
-      },
-      {
-        name: "Neutron Star",
-        description:
-          "<color=yellow>Ethereal Influence</color>'s cooldown is reduced by 5% for each enemy hit by the explosion.",
-        image: Star_Yubar_EtherealInfluenceCooldownReduction,
-      },
-      {
-        name: "Young Stars's Power",
-        description:
-          "Increase <color=yellow>Supernova</color>'s size by 20% and stuns enemies for 1 second.",
-        image: Star_Yubar_StrongerSuperNova,
-      },
-      {
-        name: "Blazing Meteor",
-        description:
-          "<color=yellow>Cataclysm</color>'s meteors scorch the ground, dealing <sprite=1><color=#16D7FF>175%</color> Light Damage over 2 seconds.",
-        image: Star_Yubar_StrongerCataclysm,
-      },
-      {
-        name: "Light Heart",
-        description:
-          "When channeling <color=yellow>Tranquility</color>, deals <sprite=1><color=#16D7FF>165%</color> damage to all colliding enemies and briefly stun them.",
-        image: Star_Yubar_TranquilityDamage,
-      },
-    ],
+    constellations: [],
   },
   Hero_Vesper: {
     name: "Vesper",
@@ -339,7 +190,7 @@ const travelers = {
     image: Hero_Aurena,
     constellations: [],
   },
-};
+} as const;
 
 /* eslint-enable unicorn/no-null */
 
@@ -360,6 +211,8 @@ export const getTravelers = () =>
     id: id as keyof typeof travelers,
     ...traveler,
   }));
+
+export const getTravelerIds = () => Object.keys(travelers) as Traveler["id"][];
 
 export const generateTravelerStats = (
   traveler: Pick<
